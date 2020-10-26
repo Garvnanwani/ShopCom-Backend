247ff9bbee17d15da3abfc1b71a47c9f751e313a
Garv nanwani <garvnanwani16@gmail.com>
2020-10-26 17:20:56 +0530

    added product route

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
+const products = require('./data/products')
 
 app.get('/', (req, res) => {
     res.send('API is running');
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+})
+
+app.get('/api/product/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product);
 })
 
 app.listen(PORT, () => {
